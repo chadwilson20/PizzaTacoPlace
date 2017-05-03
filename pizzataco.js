ab59c031f4db8b10420abfe3e57be1b3c2a8cfbe
Kevin Buffardi <kbuffardi@csuchico.edu>
2017-05-03 02:04:48 -0700

    Taking orders for tacos now

---
+++ pizzataco.js
@@ -0,0 +1,27 @@
+//we want to keep track of how many (and what kind) of pizza
+//as well as how many (and what kind) of tacos
+
+function addPizza()
+{
+  //get the status of each checkbox
+  var veg = document.getElementById("checkVeggies").checked;
+  var pepperoni = document.getElementById("checkPepperoni").checked;
+  var sausage = document.getElementById("checkSausage").checked;
+
+  //meat lovers (pepperoni & sausage),
+  //veggie (veggies),
+  //supreme (everything),
+  //cheese (nothing)
+}
+
+function addTacoPlate()
+{
+  //get the status of each checkbox
+  var steak = document.getElementById("radioSteak").checked;
+  var chicken = document.getElementById("radioChicken").checked;
+  var pork = document.getElementById("radioPork").checked;
+
+  //Carne Asada (steak),
+  //Pollo (chicken),
+  //Carnitas (pork)
+}
